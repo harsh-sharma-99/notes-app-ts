e0e8d2409bb31affe613db94a319b08e85188f19
Harsh Sharma <sharmaharsh9944@gmail.com>
2023-03-09 00:13:49 +0530

    added routing for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import './App.css';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     HI
+    <div>
+     <Routes>
+       <Route path = "/" element = {<h1>Home Page</h1>} /> 
+       <Route path = "/new" element = {<h1>New Page</h1>} /> 
+       <Route path = "/:id" >
+        <Route index element = {<h1>Show</h1>} /> 
+        <Route path = "edit" element = {<h1>Edit Page</h1>} /> 
+       </Route>
+       <Route path = "*"  element = {<Navigate to = "/" />} /> 
+     </Routes>
     </div>
   );
 }
